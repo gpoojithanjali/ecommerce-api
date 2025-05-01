cb25c934e997e29b0308192e5ca67a4ddf636b0e
gpoojithanjali <gpoojithanjali7@gmail.com>
2025-05-01 21:06:45 +0530

    Update swagger.js

--- backend/swagger.js
+++ backend/swagger.js
@@ -11,7 +11,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:5000/api",
+                url: "https://ecommerce-api-s8u7.onrender.com/api",
             },
         ],
     },
